a7a4b24bd56ff7a1255753da2f7476e9e15bedab
lecharles <charleslozano@gmail.com>
2026-06-12 16:40:44 -0700

    add observation model and initial migration

--- myworldmodel/main_app/models.py
+++ myworldmodel/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Observation(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    observed_at = models.DateTimeField()
+
+    def __str__(self): # equivalent toString
+        return self.name
