cd7eb1e9854a970c4dcc6cab8e3986b1ff63fda9
lecharles <charleslozano@gmail.com>
2026-06-12 17:18:35 -0700

    add base layout and observation index

--- myworldmodel/main_app/urls.py
+++ myworldmodel/main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('observations/', views.observations_index, name='observation-index'),
 ]
--- myworldmodel/main_app/views.py
+++ myworldmodel/main_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Observation
 
 
 def home(request):
     return HttpResponse('<h1>My World Model</h1>')
+
+def observations_index(request):
+    observations = Observation.objects.all()
+    return render(request, 'observations/index.html', {'observations': observations})
